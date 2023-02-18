9387a08b157f4e074c605ac284bb1b1c44d1fb01
MuhanedYahya <yahya.muhaned@gmail.com>
2023-02-18 11:11:48 +0300

    upgrade to https added

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -32,7 +32,14 @@
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-02VG04DTGB"></script> -->
+    <!-- ##########################################################################################
+    ##########################################################################################
+    ########################################################################################## -->
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- ##########################################################################################
+    ##########################################################################################
+    ########################################################################################## -->
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag() { dataLayer.push(arguments); }
